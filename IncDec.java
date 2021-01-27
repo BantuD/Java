360200d8425ef9ec4a29660482acbfc1cef2a418
BantuD <500069424@stu.upes.ac.in>
2021-01-27 20:46:39 +0530

    Post and Pre Increments

---
+++ IncDec.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+public class IncDec
+{
+	public static void main(String arg[])
+	{
+		int x=5,y=4,z;
+		z=2*x+++3*++y; //Here 2*x++ +3*++y; you can use it
+		System.out.println(z);
+	}
+}
+
